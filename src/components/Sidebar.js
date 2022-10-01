e73f6b6ab4c0fdfabfbe9ba5299cd94b7eb9f277
SalahSoussou <kaka79702@gmail.com>
2022-10-01 18:01:06 +0100

    ok

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div>
+      <h3>Salah</h3>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default Sidebar;
